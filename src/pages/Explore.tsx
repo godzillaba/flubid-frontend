90017a901e2aebf345e453aeed893cfb3d4a5eab
Henry <11198460+godzillaba@users.noreply.github.com>
2023-02-02 00:57:17 -0500

    example graphql query working on explore page

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import { CircularProgress } from '@material-ui/core';
+import React, { useEffect } from 'react';
+
+import {execute, TestQueryDocument, TestQueryQuery} from "../graph/.graphclient";
+
 
 export default function Explore() {
-  return (
-    <h1>Explore</h1>
-  );
+  const [data, setData] = React.useState<TestQueryQuery>();
+
+  useEffect(() => {
+    execute(TestQueryDocument, {a: 1}).then((result) => {
+      setData(result?.data)
+    })
+  }, [setData])
+
+  if (data) { 
+    return (
+      <h1>{JSON.stringify(data)}</h1>
+    );
+  }
+  else {
+    return (
+      <CircularProgress />
+    )
+  }
 }
 
